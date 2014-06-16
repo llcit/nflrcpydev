f7693df151c087cfda044991721c8b8453e9cf7a
ram <rmedina@hawaii.edu>
2014-06-16 09:11:45 -1000

    tuning submenus and header

--- static/js/site.js
+++ static/js/site.js
@@ -1,7 +1,8 @@
 // site.js
 jQuery(function($) {
     $("#sitemap_btn").click(function () {
-        $('#sitemap_panel').toggle( "blind", "down", 1500 );
+        $("#sitemap_panel").toggle( "blind", "down", 1500 );
+        $("#sitemap_btn_toggle").toggleClass("glyphicon-chevron-down glyphicon-chevron-up");
     });
 
     $(document).ready(function() {
