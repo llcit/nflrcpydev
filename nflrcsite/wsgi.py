84c6d5e43012e8aa14a3932cd0e7e5a7aa1f694e
nflrc system admin <nflrcsa@nflrc-server-x.mcl>
2014-07-29 06:29:26 -1000

    setup adjust

--- nflrcsite/wsgi.py
+++ nflrcsite/wsgi.py
@@ -1,14 +1,13 @@
 """
 WSGI config for nflrcsite project.
+NOTE: This config is NOT set up for production use. See production server setup for production use.
 """
 
 import os, sys
 
-sys.path.insert(0, '/pythonweb/nflrcpydev/')
+sys.path.insert(0, 'PATH HERE')
 
-os.environ["DJANGO_SETTINGS_MODULE"] = "nflrcsite.settings.prod-nflrc-hawaii-edu"
-
-#os.environ.setdefault("DJANGO_SETTINGS_MODULE", "nflrcsite.settings")
+os.environ["DJANGO_SETTINGS_MODULE"] = "DOTTED PATH HERE"
 
 from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
