b6f84d696e7f5353421cb683c12ccd62ee54f4ac
ram <rmedina@hawaii.edu>
2014-08-12 12:28:55 -1000

    fabfile committed this without comments.

--- static/js/site.js
+++ static/js/site.js
@@ -3,6 +3,7 @@ jQuery(function($) {
     $("#sitemap_btn").click(function () {
         $("#sitemap_panel").toggle( "blind", "down", 700 );
         $("#sitemap_btn_toggle").toggleClass("glyphicon-chevron-down glyphicon-chevron-up");
+        // $("#sitemap_panel").css( "height", "100%");
     });
 
     $(document).ready(function() {
