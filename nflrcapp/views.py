da46c384d49506e45cead67673bf06da3976a919
ram <rmedina@hawaii.edu>
2014-12-31 13:04:17 -1000

    semi integration of retrieval by tag

--- nflrcapp/views.py
+++ nflrcapp/views.py
@@ -138,13 +138,12 @@ def prodev(request, tag=None):
         listing = Prodev.objects.filter(pdtype__icontains=tag).order_by('-id')
         if not listing: # Retrieve items by tag
             listing = Prodev.objects.get_tagged_items(tag=tag, item_type='prodev')
+        
         if tag == 'upcoming':
             tag = ''
     else:
         listing = None
-
-    featured = Prodev.objects.filter(featured=True).order_by('featured_rank')
-            # listing = Prodev.objects.filter().order_by('pdtype', '-id')
+        featured = Prodev.objects.filter(featured=True).order_by('featured_rank')
 
     return render_to_response('l2-events.html', {
         'events': listing,
