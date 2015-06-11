60a7a959f6a7a7c3c70f3b0642fe34c5dbd7d719
ram <rmedina@hawaii.edu>
2015-06-10 18:29:00 -1000

    fabfile committed this without comments.

--- nflrcapp/admin.py
+++ nflrcapp/admin.py
@@ -32,7 +32,7 @@ class ContactRoleAdmin(admin.ModelAdmin):
 	ordering = ['list_rank']
 
 class ContactAdmin(admin.ModelAdmin):
-	list_display = ('last_name', 'first_name', 'title', 'bio', 'role', 'staff_role', 'listing_rank', 'image')
+	list_display = ('last_name', 'first_name', 'title', 'role', 'staff_role', 'listing_rank', 'image')
 	list_filter = ['role',]
 	list_editable = ['role', 'staff_role', 'listing_rank','image']
 	list_per_page = 200
